5c29ee2dee04ede852bdacac9a5ddb1b4ea5042c
zhangli <313890735@qq.com>
2016-03-01 08:58:55 +0800

    添加6个UIView

---
+++ Animation1/Animation1/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Animation1
+//
+//  Created by 余亮 on 16/3/1.
+//  Copyright © 2016年 余亮. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var V1: UIView!
+    
+    @IBOutlet weak var V2: UIView!
+
+    @IBOutlet weak var V3: UIView!
+    
+    @IBOutlet weak var V4: UIView!
+    
+    @IBOutlet weak var V5: UIView!
+    
+    @IBOutlet weak var V6: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
